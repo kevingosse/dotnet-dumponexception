52deb7410d8bb1823eee847bc6eb510d7a5eaefb
Tony Redondo <tonyredondo@gmail.com>
2020-12-23 11:46:14 +0100

    Initial solution file and folder structure (#1)
    * Initial folder structure
    
    * Common props
    
    * Adds missing PaskAsTool prop.
    
    * Initial commandline parser, and initial startup hook class

---
+++ src/DumpOnException.CLI/Options.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System.Collections.Generic;
+
+namespace DumpOnException.CLI
+{
+    internal class Options
+    {
+        [Usage(ApplicationAlias = "dotnet dumponexception")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("Example 1", UnParserSettings.WithUseEqualTokenOnly(), new Options { Value = new string[] { "test" } });
+                yield return new Example("Example 2", UnParserSettings.WithUseEqualTokenOnly(), new Options { Filter = "(A|B|C)", Value = new string[] { "test" } });
+            }
+        }
+
+        [Option("filter", Required = false, HelpText = "Applies a regex exception filter to trigger the dump.")]
+        public string Filter { get; set; }
+
+        [Value(0, Hidden = true, Required = true, HelpText = "Command to be wrapped by the cli tool.")]
+        public IEnumerable<string> Value { get; set; }
+    }
+}
---
+++ src/DumpOnException.CLI/Program.cs
@@ -0,0 +1,60 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DumpOnException.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser parser = new Parser(settings =>
+            {
+                settings.AutoHelp = true;
+                settings.AutoVersion = true;
+                settings.EnableDashDash = true;
+                settings.HelpWriter = null;
+            });
+
+            ParserResult<Options> result = parser.ParseArguments<Options>(args);
+            result.MapResult(ParsedOptions, errors => ParsedErrors(result, errors));
+        }
+
+        private static int ParsedOptions(Options options)
+        {
+            // Process options
+            
+            return 0;
+        }
+
+        private static int ParsedErrors(ParserResult<Options> result, IEnumerable<Error> errors)
+        {
+            HelpText helpText = null;
+            if (errors.IsVersion())
+            {
+                helpText = HelpText.AutoBuild(result);
+            }
+            else
+            {
+                helpText = HelpText.AutoBuild(
+                    result,
+                    h =>
+                    {
+                        h.Heading = "DumpOnException";
+                        h.AddNewLineBetweenHelpSections = true;
+                        h.AdditionalNewLineAfterOption = false;
+                        return h;
+                    },
+                    e =>
+                    {
+                        return e;
+                    });
+            }
+
+            Console.WriteLine(helpText);
+            return 1;
+        }
+    }
+}
