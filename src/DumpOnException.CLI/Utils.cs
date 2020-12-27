89fee6f7e7cde06110d5bafe54d107bdb2413191
Kevin Gosse <krix33@gmail.com>
2020-12-27 13:48:07 +0100

    Don't redirect input/output (#5)

--- src/DumpOnException.CLI/Utils.cs
+++ src/DumpOnException.CLI/Utils.cs
@@ -12,11 +12,7 @@ namespace DumpOnException.CLI
         {
             ProcessStartInfo processInfo = new ProcessStartInfo(filename)
             {
-                RedirectStandardError = true,
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
                 UseShellExecute = false,
-                CreateNoWindow = true,
                 WorkingDirectory = Environment.CurrentDirectory,
             };
 
@@ -42,30 +38,14 @@ namespace DumpOnException.CLI
                 using (Process childProcess = new Process())
                 {
                     childProcess.StartInfo = startInfo;
-                    childProcess.OutputDataReceived += (sender, e) =>
-                    {
-                        if (e.Data != null)
-                            Console.WriteLine(e.Data);
-                    };
-                    childProcess.ErrorDataReceived += (sender, e) =>
-                    {
-                        if (e.Data != null)
-                            Console.Error.WriteLine(e.Data);
-                    };
                     childProcess.EnableRaisingEvents = true;
                     childProcess.Start();
-                    childProcess.BeginOutputReadLine();
-                    childProcess.BeginErrorReadLine();
 
                     using (cancellationToken.Register(() =>
                     {
                         try
                         {
-                            if (childProcess != null)
-                            {
-                                childProcess.StandardInput.Close();
-                                childProcess.Kill();
-                            }
+                            childProcess.Kill();
                         }
                         catch
                         {
